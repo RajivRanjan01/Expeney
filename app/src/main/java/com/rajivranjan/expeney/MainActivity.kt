fc6020fc221d611937b566cacc063a288aec0327
RajivRanjan01 <rajivranjan12142@gmail.com>
2025-07-07 23:22:23 +0530

    feat: create input UI for adding expenses

--- app/src/main/java/com/rajivranjan/expeney/MainActivity.kt
+++ app/src/main/java/com/rajivranjan/expeney/MainActivity.kt
@@ -4,13 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.graphics.Color
 import com.rajivranjan.expeney.ui.theme.ExpeneyTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +34,63 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ExpeneyTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                ExpenseInputScreen()
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+
+
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    ExpeneyTheme {
-        Greeting("Android")
+fun ExpenseInputScreen() {
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+
+    ){
+
+
+        Text(
+            text =" Track Your Expense \uD83D\uDCB8",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color(0xFF2196F3)
+        )
+
+
+        var expenseName by remember { mutableStateOf("") }
+
+        var expenseAmount by remember { mutableStateOf("") }
+
+
+        TextField(
+            value = expenseName,
+            onValueChange = { expenseName = it },
+            label = { Text("Enter expense name") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 16.dp)
+        )
+
+        TextField(
+            value = expenseAmount,
+            onValueChange = { expenseAmount = it },
+            label = { Text("Enter expense amount") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 16.dp)
+        )
+        Spacer(modifier = Modifier.padding(24.dp))
+
+        Button(onClick = {},
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 8.dp)) { Text(text = "Add Expense") }}
     }
-}
+
